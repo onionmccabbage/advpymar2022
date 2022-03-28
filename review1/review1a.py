1591938f6d2ace5d0b92ebe1cb0ada7bd8990f54
onionmccabbage <onionmccabbage@live.com>
2022-03-28 13:59:16 +0100

    Add files via upload

---
+++ review1/review1a.py
@@ -0,0 +1,36 @@
+from my_decorators import show_args
+
+def root(x):
+    return x**0.5 # anything to the power of 0.5 is the square root
+
+
+@show_args
+def mapRoots(start, number):
+    '''
+    Return the square root of integers between min and max
+    '''
+    print('Using Map')
+    # roots = map(lambda x: x**0.5, range(start,number+1))
+    roots = list( map(root, range(start,number+1)) )
+    print(roots)
+
+# def compRoots(start, number):
+#     print('Using List comprehension')
+#     roots = [x**0.5 for x in range(start,number+1)]
+#     for root in roots:
+#         print (root)
+
+# def genRoots(start, number):
+#     print('Using Generator comprehension')
+#     roots = (x**0.5 for x in range(start,number+1))
+#     for root in roots:
+#         print (root)
+
+if __name__ == '__main__':
+    number = 0
+    while number<1:
+        number = int(float(input('Enter number : ')))
+    start = 0
+    mapRoots(start, number)
+    # compRoots(start, number)
+    # genRoots(start, number)
