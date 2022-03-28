ab7977152e4eff301aca53019bdee9fa07493714
onionmccabbage <onionmccabbage@live.com>
2022-03-28 12:31:50 +0100

    Add files via upload

--- some_classes/a.py
+++ some_classes/a.py
@@ -1,9 +1,27 @@
-class Shape():
-    def __init__(self, n):
+# we can use the Abstract Base Class (ABC)
+from abc import ABCMeta, abstractmethod, abstractproperty
+
+# here is an abstract BASE class
+class AbstractShape():
+    __metaclass_ = ABCMeta # ..from the abc imports
+    @abstractmethod
+    def __str__(self):
+        pass
+    @property
+    @abstractmethod
+    def name():
+        pass # no body to the method
+
+# here is an abstract class
+class Shape(AbstractShape): # our class implements our Abstract Base Class
+    def __init__(self, n, sides):
         self.name=n
+        self.sides = sides # NB this is a directly accessible property
     @property
     def name(self):
         return self.__name
     @name.setter
     def name(self, n):
         self.__name = n
+    def __str__(self):
+        return 'This shape is called {} with {} sides'.format(self.name, self.sides)
--- some_classes/c.py
+++ some_classes/c.py
@@ -1,9 +1,22 @@
 from a import Shape
 
+
+# here is a concrete class
 class Circle(Shape): # this class inherits from 'Shape'
-    def __init__(self, n):
-        self.name = n
+    def __init__(self, n, sides):
+        self.name = n # or we could call super
+        self.sides = '1 or infinite, depending on your view'
+
+class Square(Shape):
+    def __init__(self, n, sides):
+        # we can call methods in the parent class
+        super().__init__(n, sides)
+        # self.name = n
 
 if __name__ == '__main__':
-    c = Circle('claire')
+    c = Circle('claire', 1)
+    s = Square('Reg', 4)
     print( c.name )
+
+
+    print(s, c)
